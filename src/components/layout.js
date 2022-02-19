08473488d812b687f959a49fff412fe153e6afb7
Ben Wozniak <benrwozniak@gmail.com>
2022-02-18 21:20:24 -0600

    centered header and menu, scaled up header sizes, cleanup css

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,36 +24,34 @@ const Layout = ({ pageTitle, children }) => {
         <div className={container}>
             <title>{pageTitle} | {data.site.siteMetadata.title}</title>
             <header className={siteTitle}><h1>{data.site.siteMetadata.title}</h1></header>
-            <body>
-                <nav>
-                    <ul className={navLinks}>
-                        <li className={navLinkItem}>
-                            <Link to="/" className={navLinkText}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className={navLinkItem}>
-                            <Link to="/about" className={navLinkText}>
-                                About
-                            </Link>
-                        </li>
-                        <li className={navLinkItem}>
-                            <Link to="/blog" className={navLinkText}>
-                                Blog
-                            </Link>
-                        </li>
-                        <li className={navLinkItem}>
-                            <Link to="/production" className={navLinkText}>
-                                $ Production
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-                <main>
-                    <h2 className={heading}>{pageTitle}</h2>
-                    {children}
-                </main>
-            </body>
+            <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link to="/" className={navLinkText}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/about" className={navLinkText}>
+                            About
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>
+                            Blog
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/production" className={navLinkText}>
+                            Production
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <main>
+                <h2 className={heading}>{pageTitle}</h2>
+                {children}
+            </main>
         </div>
     )
 }
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,8 @@
 import Typography from "typography";
 import oceanTheme from 'typography-theme-ocean-beach';
 
+oceanTheme.scaleRatio = 3;
+
 const typography = new Typography(
     oceanTheme
 );
