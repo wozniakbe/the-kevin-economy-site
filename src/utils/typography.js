db540409f717836cd00427a1a239a57b045024c7
Ben Wozniak <benrwozniak@gmail.com>
2022-08-21 13:12:24 -0500

    wrote the article, yo (#11)

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,7 +3,7 @@ import oceanTheme from 'typography-theme-ocean-beach';
 
 oceanTheme.scaleRatio = 3;
 oceanTheme.overrideThemeStyles = ({ rhythm }, options) => ({
-  'h1,h2,h3,h4,h5, hr': {
+  'h1,h2,h3,h4,h5,h6, hr': {
     marginBottom: rhythm(1 / 2),
     marginTop: rhythm(1 / 2),
   }
